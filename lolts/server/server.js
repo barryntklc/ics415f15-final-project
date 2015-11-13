991cc013cc2985befabe654d45719de8ea98dde4
Chunmeista <barryn.chun@gmail.com>
2015-11-13 01:39:52 -1000

    initial commit 04

---
+++ lolts/server/server.js
@@ -0,0 +1,10 @@
+//http://stackoverflow.com/questions/23507384/adding-more-fields-to-meteor-user-accounts
+
+
+//Accounts.onCreateUser(function (options, user) {
+  //  alert("hello");
+    /*alert(options);
+    user.profile['type'] = "user";
+
+    return user;*/
+//});
