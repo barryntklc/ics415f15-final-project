5adeb59aa1c9cdff0c5105216fd5462a5faf0d0d
Chunmeista <barryn.chun@gmail.com>
2015-12-01 10:44:05 -1000

    logic 01

--- lolts/collections/participants.js
+++ lolts/collections/participants.js
@@ -1,1 +1,7 @@
 Participants = new Mongo.Collection('participants');
+
+var participant = {
+    summoner: "",
+    tier: 0,
+    rank: 0
+}
---
+++ lolts/server/logic.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Chunmeista on 12/1/2015.
+ */
+
+var tier_alpha = ["Unranked", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Challenger"];
+var tier_value = [1, 2, 3, 4, 5, 6, 7, 8];
+
+var rank_alpha = ["V", "IV", "III", "II", "I"];
+var rank_value = [0.0, 0.2, 0.4, 0.6, 0.8];
+
